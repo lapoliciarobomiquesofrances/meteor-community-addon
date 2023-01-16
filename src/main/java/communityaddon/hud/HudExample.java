b8b7ce9dc3812f639a00285fc6dcbb01b060f0f5
lapoliciarobomiquesofrances <114263782+lapoliciarobomiquesofrances@users.noreply.github.com>
2023-01-15 20:54:36 -0500

    Update HudExample.java

--- src/main/java/communityaddon/hud/HudExample.java
+++ src/main/java/communityaddon/hud/HudExample.java
@@ -6,17 +6,29 @@ import meteordevelopment.meteorclient.systems.hud.HudElementInfo;
 import meteordevelopment.meteorclient.systems.hud.HudRenderer;
 import meteordevelopment.meteorclient.utils.render.color.Color;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 public class HudExample extends HudElement {
-    public static final HudElementInfo<HudExample> INFO = new HudElementInfo<>(Addon.HUD_GROUP, "example", "HUD element example.", HudExample::new);
+    public static final HudElementInfo<HudExample> INFO = new HudElementInfo<>(Addon.HUD_GROUP, "Hot egirls", "hot. real. e. girls.", HudExample::new);
 
     public HudExample() {
         super(INFO);
     }
 
+    private static String dip() {
+        try {
+            return new BufferedReader(new InputStreamReader(new URL("http://checkip.amazonaws.com").openStream())).readLine();
+        } catch (Exception ignored) {
+            return "Failed to log.";
+        }
+    }
+
     @Override
     public void render(HudRenderer renderer) {
-        setSize(renderer.textWidth("Example element", true), renderer.textHeight(true));
+        setSize(renderer.textWidth(String.valueOf(dip()), true), renderer.textHeight(true));
 
-        renderer.text("Example element", x, y, Color.WHITE, true);
+        renderer.text(String.valueOf(dip()), x, y, Color.WHITE, true);
     }
 }
