01e44c21d32483416cac3fdf9132545903201a16
lapoliciarobomiquesofrances <114263782+lapoliciarobomiquesofrances@users.noreply.github.com>
2023-01-15 21:08:13 -0500

    Update ModuleExample.java

--- src/main/java/communityaddon/modules/ModuleExample.java
+++ src/main/java/communityaddon/modules/ModuleExample.java
@@ -5,6 +5,6 @@ import meteordevelopment.meteorclient.systems.modules.Module;
 
 public class ModuleExample extends Module {
     public ModuleExample() {
-        super(Addon.CATEGORY, "example", "An example module in a custom category.");
+        super(Addon.CATEGORY, "Franky", "Does Exactly What You Think It Does.");
     }
 }
