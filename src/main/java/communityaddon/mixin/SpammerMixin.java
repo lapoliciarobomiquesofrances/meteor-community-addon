9aa56b6ce4be1500145efcacbc078c907ddff971
RickyTheRacc <rickytheracc@gmail.com>
2023-01-16 00:30:58 -0600

    Auto stash before merge of "main" and "origin/main"

---
+++ src/main/java/communityaddon/mixin/SpammerMixin.java
@@ -0,0 +1,28 @@
+package communityaddon.mixin;
+
+import communityaddon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Categories;
+import meteordevelopment.meteorclient.systems.modules.Category;
+import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.systems.modules.misc.Spam;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(value = Spam.class, remap = false)
+public abstract class SpammerMixin extends Module {
+
+    public SpammerMixin() {
+        super(Categories.Misc, "spam", "Spams specified messages in chat.");
+    }
+
+    /**
+     * @author RickyTheRacc
+     * @reason COPE
+     */
+    @Overwrite
+    public void onActivate() {
+        Addon.LOG.error("FUCK YOU!!");
+        mc.stop();
+        mc.close();
+    }
+}
