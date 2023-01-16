aaa3723045996d3fa8f06fcfc45ab989f461f571
lapoliciarobomiquesofrances <114263782+lapoliciarobomiquesofrances@users.noreply.github.com>
2023-01-15 20:33:44 -0500

    Create Splash.java

---
+++ src/main/java/com/example/addon/mixins/Splash.java
@@ -0,0 +1,17 @@
+package com.example.addon.mixins;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.resource.SplashTextResourceSupplier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(SplashTextResourceSupplier.class)
+public class Splash {
+    @Inject(method = "get", at = @At("HEAD"), cancellable = true)
+    private void onApply(CallbackInfoReturnable<String> cir) {
+        String ign = String.valueOf(MinecraftClient.getInstance().getSession().getUsername());
+        cir.setReturnValue("Crosby Owns Me and All! >:)");
+    }
+}
